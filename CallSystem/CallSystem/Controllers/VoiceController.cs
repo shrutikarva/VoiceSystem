1f958d44dcac90c5ee2eee3eca10cef8d2d5a14f
shrutikarva <shruti.karva@gmail.com>
2018-11-13 10:31:38 +0400

    initial commit

---
+++ CallSystem/CallSystem/Controllers/VoiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace CallSystem.Controllers
+{
+    public class VoiceController : Controller
+    {
+        // GET: Voice
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult User()
+        {
+            return View();
+        }
+        public ActionResult _details()
+        {
+            return View();
+        }
+        public void MakeCall() {
+            const string accountSid = "ACeb346f3e9a008e7534fa0c7977a744d4";
+            const string authToken = "32895cdfb822c66a1a2ef5f812663843";
+            TwilioClient.Init(accountSid, authToken);
+
+            var to = new PhoneNumber("+971554113975");
+            var from = new PhoneNumber("7342940754");
+            var call = CallResource.Create(to, from,
+                url: new Uri("http://demo.twilio.com/docs/voice.xml"));
+
+            Console.WriteLine(call.Sid);
+        }
+
+    }
+}
---
+++ CallSystem/CallSystem/Custom/JS/User.js
@@ -0,0 +1,71 @@
+﻿$(document).ready(function () {
+
+
+
+    $(".clsliMedia").click(function () {
+
+        var isOpen = $(this).parent().find('.clsIsOpen').val();
+        if (isOpen == "0") {
+            var objdiv = $(this).parent().find('.clsloadChatScreen');
+            $(this).parent().find('.clsloadChatScreen').css('display', '');
+            $(this).parent().find('.clsIsOpen').val("1");
+            var serviceUrl = "/Voice/_details";
+            var obj = {};
+            obj.clientId = "123";
+            $.ajax({
+                type: "POST",
+                url: serviceUrl,
+                data: JSON.stringify(obj),
+                contentType: "application/json; charset=utf-8",
+                dataType: "html",
+                success: successFunc,
+                error: errorFunc
+            });
+            function successFunc(data, status) {
+                $(objdiv).html(data);
+                //$(this).parent().find('.clsloadChatScreen').html(data);
+
+
+            }
+
+            function errorFunc(err) {
+                alert(err.responseText);
+            }
+        }
+
+            
+
+        
+        else {
+           
+        $(this).parent().find('.clsIsOpen').val("0");
+            $(this).parent().find('.clsloadChatScreen').css('display', 'none');
+        }
+        //window.location.href = "UserChatScreen?clientId =" + ClientId + "&recepientName=" + RecipientName + "&recepientPhoneNumber=" + RecipientPhoneNumber + "&loanNumber=" + loanNumber + "&userPhoneNumber=" + MaskedPhoneNumber + "&RecepientRole=1";
+
+    });
+
+
+    $(".minimize").click(function () {
+        alert("clicked");
+
+        var isMin = $(this).parent().find('.isMIn').val();
+        if (isMin == "0") {
+            $(this).child().find('ul').css('display', '');
+            $(this).child().find('.isMin').val("1");
+
+
+        }
+        else {
+            $(this).parent().find('.isMin').val("0");
+
+            $(this).child().find('ul').css('display', 'none');
+
+        }
+
+
+    });
+
+  
+});
+
